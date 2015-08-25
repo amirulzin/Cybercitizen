40a5c4ff9ed6e8580f0a2cfd8263f4875dc5b93a
Amirul Zin <amirul.mdzin@gmail.com>
2015-08-26 00:49:29 +0800

    Hold off using transparent version

---
+++ app/src/main/java/com/opensource/cybercitizen/activity/model/DTSBus.java
@@ -0,0 +1,15 @@
+package com.opensource.cybercitizen.activity.model;
+
+
+public class DTSBus
+{
+    private String station;
+    private String locationHint;
+    private double latitude;
+    private double longitude;
+
+    private static class Dataset
+    {
+
+    }
+}
